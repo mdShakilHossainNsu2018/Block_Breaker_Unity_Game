0c495c66e7d45b99f212c68b5a4c518b5dac9b7a
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2021-07-02 02:30:46 +0600

    game speed set

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,23 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Paddle paddle1;
 
-    private Vector2 paddleToBallVector;
+    private Vector2 m_PaddleToBallVector;
 
     private Rigidbody2D m_Rigidbody2D;
     private bool m_HasStarted = false;
     [SerializeField] private float xPush = 2f;
     [SerializeField] private float yPush = 15f;
+    [SerializeField] private AudioClip[] ballSounds;
+
+    private AudioSource m_AudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-        paddleToBallVector = transform.position - paddle1.transform.position;
+        m_PaddleToBallVector = transform.position - paddle1.transform.position;
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -44,6 +50,17 @@ public class Ball : MonoBehaviour
     private void LockBallToPaddle(){
         var position = paddle1.transform.position;
         Vector2 paddlePos = new Vector2(position.x, position.y);
-        transform.position = paddlePos + paddleToBallVector;
+        transform.position = paddlePos + m_PaddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Debug.Log(m_HasStarted.ToString());
+        if (m_HasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            m_AudioSource.PlayOneShot(clip);  
+        }
+        
     }
 }
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,10 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
+    private Level m_Level;
+
+    private void Start()
+    {
+        m_Level = FindObjectOfType<Level>();
+        m_Level.CountBreakableBlock();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
+        if (breakSound !=null)
+        {
+            if (Camera.main is { }) AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+
+        }
         Destroy(gameObject);
+        m_Level.BlockDestroyed();
     }
 }
